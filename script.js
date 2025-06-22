6fe6917e5252a43c53df17d2c5970c13ac95bc71
tikrack <tikrackcode@gmail.com>
2025-06-22 18:47:25 +0330

    files -> init

---
+++ script.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+
+async function downloadFlags() {
+    try {
+        const jsonData = fs.readFileSync('./country.json', 'utf8');
+        const countries = JSON.parse(jsonData);
+
+        if (!fs.existsSync('./Pic')) {
+            fs.mkdirSync('./Pic');
+        }
+
+        for (const country of countries) {
+            const imgAddress = country.flags.svg;
+            const fileName = imgAddress.split('/').pop();
+            const filePath = path.join('./Pic', fileName);
+
+            try {
+                const response = await axios.get(imgAddress, { responseType: 'arraybuffer' });
+                fs.writeFileSync(filePath, response.data);
+                console.log(`Downloaded: ${fileName}`);
+            } catch (error) {
+                console.error(`Failed to download ${imgAddress}:`, error.message);
+            }
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+downloadFlags();
---
+++ script.php
@@ -0,0 +1,15 @@
+<?php
+    $json = file_get_contents("./country.json");
+    $json = json_decode($json);
+
+    foreach($json as $country){
+        $img_address = $country->flags->svg;
+        $file_name = explode("/", $img_address);
+        $file_name = end($file_name);
+
+        $fileContents = file_get_contents($img_address);
+
+        if ($fileContents !== false) {
+            file_put_contents("./Pic/" . $file_name , $fileContents);
+        }
+    }
